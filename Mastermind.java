e3561fb5342eed7b1a66f7f007d5198f8d2a3b6b
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2022-02-01 09:35:46 -0500

    Update Mastermind.java

--- Mastermind.java
+++ Mastermind.java
@@ -6,58 +6,83 @@ public class Mastermind
 {
 	ArrayList<String> test = new ArrayList<String>();
 	//2d array list
-	ArrayList<ArrayList<String>>board = new ArrayList<ArrayList<String>>();
+	
 	//How will human addd a four digit color band?
-	int[] answers = new int[6];
-	int[] userinputs = new int[6];
+	int[] answers = new int[4];
+	int[] userinputs = new int[4];
+	int failcount = 0;
+	//Number version of mastermind. 
+	//answers array is populated with random numbers from 1 to 6
+	//userinputs array is populated by user inputs: values are from 1 to 6
+	
 	public void populateBoard()
 	{
 		//Colors are red, green, blue, yellow, cyan and purple
 		//number 1 equals red, 2 equals green, 3 equals blue, four equals yellow five equals cyan six equals purple
-		for(int i=0; i<6; i++)
+		for(int z=0; z<answers.length; z++)
 		{
-			int numguess = (int)(Math.random()*5)+1;
-			answers[i] = numguess;
-		}
-		for( int x=0; x<6; x++)
-		{
-			Scanner scan = new Scanner(System.in);
-			do{
-				System.out.println("Enter your nummber 1 through 6");
-		        int picknum = scan.nextInt();
-		      }while(picknum<1 || picknum>5);
-		      userinputs[x]=picknum;
-		for(int n=0; n<6; n++)
-		{
-			for(int b=0; b<6; b++)
+			for(int i=z+1; i<answers.length; i++)
 			{
-				if(userinputs[b]==answers[i])
+				while(answers[z]==answers[i])
 				{
-					System.out.println("This value is in right position");
+					int numguess = (int)(Math.random()*5)+1;
+					answers[i] = numguess;
 				}
-				else if(userinputs[b]!=answers[i])
-				{
-					System.out.println("This is in wrong position");
-				}
-		     }
+			}
 		}
+		for( int x=0; x<4; x++)
+		{
+			for(int y=x+1; y<answer.length; y++)
+			{
+				while(answers[x]==userinputs[y])
+				{
+					Scanner scan = new Scanner(System.in);
+			
+					System.out.println("Enter your nummber 1 through 6");
+					int picknum = scan.nextInt();
+		    
+					userinputs[x]=picknum;
+				}
+			}
+		}
+		for(int n=0; n<answers.length; n++)
+		{
+			System.out.print("\n");
+			System.out.println(answers[n]);
+		}
+		
 		    
 		
-		}
-			
-		
-		
-	
-		
-		
-	
-		
 	}
-	public void printBoard()
+	public void checkBoards()
 	{
-		for(int i=0; i<board.size(); i++)
+		for(int k=0; k<userinputs.length; k++)
 		{
-			System.out.println(board.get(i));
+			if(userinputs[k]==answers[k])
+			{
+				System.out.print("These numbers are in the correct position" + userinputs[k]);
+			}
+			else if(userinputs[k]!=answers[k])
+			{
+				System.out.print("These numbers are in the incorrect position" + userinputs[k]);
+				failcount++;
+			}
+		
+	    }
+	    for(int n=0; n<4; n++)
+	    {
+			for(int j=n+1; j<4; j++)
+			{
+				if(answers[n]==userinputs[j])
+				{
+					System.out.print("\n");
+					System.out.println("These numbers overlap" + " " + answers[n]);
+				}
+			}
 		}
-	}
+	 }  
+	 		
+	
 }
+
+
