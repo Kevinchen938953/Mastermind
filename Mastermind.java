71eff56bc79895a1ab234243c6b9b029b9298de2
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2022-02-03 22:02:47 -0500

    Update Mastermind.java

--- Mastermind.java
+++ Mastermind.java
@@ -11,9 +11,24 @@ public class Mastermind
 	int[] answers = new int[4];
 	int[] userinputs = new int[4];
 	int failcount = 0;
-	//Number version of mastermind. 
-	//answers array is populated with random numbers from 1 to 6
-	//userinputs array is populated by user inputs: values are from 1 to 6
+	/*Number version of mastermind. 
+	answers array is populated with random numbers from 1 to 6
+	userinputs array is populated by user inputs: values are from 1 to 6
+	*/
+	public void userinterface()
+	{
+		System.out.println("Welcome to Mastermind!");
+		System.out.println("\n");
+		System.out.println("You will guess values from 1 to 6 where each number represents a distinct color");
+		System.out.println("\n");
+		System.out.println("The computer will guess four random values from 1 to 6. You will need to guess the numbers in this order");
+		System.out.println("\n");
+		System.out.println("You will receive a W for White pin if you have guessed the correct number in wrong position");
+		System.out.print("\n");
+		System.out.println("You will receive a R for Red Pin if you guessed a value in right position");
+		System.out.println("\n");
+		System.out.println("You have six attempts to guess the correct order. If you do, then you win");
+	}
 	
 	public void populateBoard()
 	{
@@ -26,46 +41,45 @@ public class Mastermind
 				while(answers[z]==answers[i])
 				{
 					int numguess = (int)(Math.random()*5)+1;
-					answers[i] = numguess;
+					answers[i] = numguess;//Populates answers array with random numbers. No repeats are allowed
 				}
 			}
 		}
+	}
+	public void populateUser()
+	{
+	
 		for( int x=0; x<4; x++)
 		{
-			for(int y=x+1; y<answer.length; y++)
-			{
-				while(answers[x]==userinputs[y])
-				{
+			
+				
 					Scanner scan = new Scanner(System.in);
 			
 					System.out.println("Enter your nummber 1 through 6");
 					int picknum = scan.nextInt();
 		    
-					userinputs[x]=picknum;
-				}
-			}
+					userinputs[x]=picknum;//user input array is populated
+				
+			
 		}
-		for(int n=0; n<answers.length; n++)
-		{
-			System.out.print("\n");
-			System.out.println(answers[n]);
-		}
+	}
+		
 		
 		    
 		
-	}
+	
 	public void checkBoards()
 	{
 		for(int k=0; k<userinputs.length; k++)
 		{
-			if(userinputs[k]==answers[k])
+			if(userinputs[k]==answers[k])//Returns all values that overlap
 			{
-				System.out.print("These numbers are in the correct position" + userinputs[k]);
+				System.out.println("R" + " "  + userinputs[k]);
 			}
 			else if(userinputs[k]!=answers[k])
 			{
-				System.out.print("These numbers are in the incorrect position" + userinputs[k]);
-				failcount++;
+				System.out.println("No Pin" + " " + userinputs[k]);//Returns all numbers that don't belong in the array
+				
 			}
 		
 	    }
@@ -76,13 +90,24 @@ public class Mastermind
 				if(answers[n]==userinputs[j])
 				{
 					System.out.print("\n");
-					System.out.println("These numbers overlap" + " " + answers[n]);
+					System.out.println("W" + " " + answers[n]);//Determines the number that do overlap
 				}
 			}
 		}
-	 }  
+		if(answers[0]==userinputs[0] && answers[1]==userinputs[1] && answers[2]==userinputs[2] && answers[3]==userinputs[3])
+		{
+					System.out.println("You win the game");//If all values in arrays are the same, then win
+		}
+		else if(answers[0]!=userinputs[0] || answers[1]!=userinputs[1] || answers[2]!=userinputs[2] || answers[3]!=userinputs[3])
+				{
+					failcount++;//If they don't match, then the player needs to guess again. Fail count goes up until 6.
+				}
+			
+	 }
+	 
+		
+	 	   
 	 		
 	
 }
-
 
