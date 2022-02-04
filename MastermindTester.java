b38ff799b3b87c9325d83779c2c8eefdd5d23077
Kevinchen938953 <59925517+Kevinchen938953@users.noreply.github.com>
2022-02-03 23:04:40 -0500

    Update MastermindTester.java

--- MastermindTester.java
+++ MastermindTester.java
@@ -2,17 +2,17 @@ public class MastermindTester
 {
 	public static void main(String[] args)
 	{
-		Mastermind game = new Mastermind();
-		game.userinterface();
+		Mastermind game = new Mastermind();//Construct an object of class Mastermind
+		game.userinterface();//Call these two methods first
 		game.populateBoard();
 		while (game.failcount!=6)
 		{
-			game.populateUser();
+			game.populateUser();//Repeat user guesses and checking until attempts run out
 			game.checkBoards();
 		}
 		if(game.failcount==6)
 		{
-			System.out.println("You lose");
+			System.out.println("You lose");//When attempts run out, player loses the game. 
 		}
 		
 	}
